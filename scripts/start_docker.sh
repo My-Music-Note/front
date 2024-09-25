d80e6deeb731d764896c40e3c954284dbd6c9718
masiljangajji <xmfpdlsj0508@gmail.com>
2024-09-25 21:12:13 +0900

    feat: start_docker.sh 컨테이너 삭제 부분 제거

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# 기존 컨테이너 중지
-docker stop masiljangajji/front-app:latest || true
-# 기존 컨테이너 삭제
-docker rm masiljangajji/front-app:latest || true
-
 # 새로운 컨테이너 실행
-docker run -d -p 80:3000 masiljangajji/front-app:latest
+docker run -d --name front-app -p 80:3000 masiljangajji/front-app:latest
